ac299b39231d9fd2bb30242e274a3fc66e2e2e25
Y.T <sasaovch@gmail.com>
2022-05-04 17:37:28 +0300

    Add files via upload

---
+++ WishBirthDay.java
@@ -0,0 +1,230 @@
+public class WishBirthDay {
+    public static void main(String[] args){
+        System.out.println("\n\n");
+        System.out.println("\t\t\t\t\t\t\t\t\t\t  To:- Андрюша");
+        System.out.println("\t\t\t\t\t\t\t\t\t\t  From:- Саша");
+        System.out.println("\t\t\t\t\t\t\t\t\t    ________________________________");
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        
+        System.out.println("\t\t\t\t\t\t\t\t\t\t\ti   i   i");
+        java.awt.Toolkit.getDefaultToolkit().beep();
+        sleep();
+        System.out.println("\t\t\t\t\t\t\t\t\t\t\ti   i   i");
+        java.awt.Toolkit.getDefaultToolkit().beep();
+        sleep();
+        System.out.println("\t\t\t\t\t\t\t\t\t\t      __i___i___i__");
+        java.awt.Toolkit.getDefaultToolkit().beep();
+        sleep();
+        System.out.println("\t\t\t\t\t\t\t\t\t\t     :\u2764           \u2764 :");
+        java.awt.Toolkit.getDefaultToolkit().beep();
+        sleep();
+        System.out.println("\t\t\t\t\t\t\t\t\t\t  ___: \u2764 21th Apr \u2764 :___");
+        java.awt.Toolkit.getDefaultToolkit().beep();
+        sleep();
+        System.out.println("\t\t\t\t\t\t\t\t\t\t : \u2764    \u2764      \u2764    \u2764   :");
+        java.awt.Toolkit.getDefaultToolkit().beep();
+        sleep();
+        System.out.println("\t\t\t\t\t\t\t\t\t     ____:\u2764     \u2763 HAPPY \u2763       \u2764:____");
+        java.awt.Toolkit.getDefaultToolkit().beep();
+        sleep();
+        System.out.println("\t\t\t\t\t\t\t\t\t    : \u2764        \u2763 BIRTHDAY  \u2763      \u2764  :");
+        java.awt.Toolkit.getDefaultToolkit().beep();
+        sleep();
+        System.out.println("\t\t\t\t\t\t\t\t\t    : \u2764    \u2764    \u2764     \u2764    \u2764   \u2764  :");
+        java.awt.Toolkit.getDefaultToolkit().beep();
+       
+        // to make the beep sound wwe have used java.awt.Toolkit.getDefaultToolkit().beep()
+    
+        sleep();
+        System.out.println("\t\t\t\t\t\t\t\t\t    : \u2764     \u2764    АНДРЕЙ    \u2764     \u2764  :");
+        java.awt.Toolkit.getDefaultToolkit().beep();
+        sleep();
+        System.out.println("\t\t\t\t\t\t\t\t\t     :................................:");
+        java.awt.Toolkit.getDefaultToolkit().beep();
+        sleep();
+
+        System.out.println("\t\t\t\t\t\t\t\t _________________________________________________________");
+        System.out.println("\n");
+        sleep();
+        System.out.println("\033[0;1m"+"\n\t\t\t\t\t\t\t\t\t\t* С ДНЕМ РОЖДЕНИЯ АНДРЕЙ!!! *");
+
+        //to bold the text we have used ANSI number "\033[0;1m"
+
+        System.out.print("\033[0;0m" +"");
+        sleep();
+        System.out.println("\n\t\t\t\t\t\t\t\t Желаю тебе прекрасного, веселого настроения.");
+        sleep();
+        System.out.println("\n\t\t\t\t\t\t\t Жалько, что не могу поздравить тебя лично,\n\t\t\t\t\t\t\t но я от всей души желаю тебе крепкого здоровья,");
+        sleep();
+        System.out.println("\n\t\t\t\t\t\t\t Чтобы зрение не падало и ты мог дальше спокойно играть,\n\t\t\t\t\t\t\t чтобы учеба не мешала заниматься тем, что нравиться.");
+        sleep();
+        System.out.println("\n\t\t\t\t\t\t\t         Но не забывай учиться, пусть и не в школею\n\t\t\t\t\t\t\t         Найди дело, которое тебе нравиться и развивайся.");
+        sleep();
+        System.out.println("\n\t\t\t\t\t\t\t        Развивайся всегда и везде, не останавливайся на достигнутом!!!\n\t\t\t\t\t\t\t                      Но не забывай и о развлечениях (Они тоже важны)");
+        sleep();
+        System.out.println("\n\t\t\t\t\t\t\t           Строй грандиозные планы, ставь невероятные задачи,\n\t\t\t\t\t\t\t                и достигай всего, чего хочешь.");
+        sleep();
+        System.out.println("\n\t\t\t\t\t\t\t                               БУДЬ СОБОЙ И НИ НА КОГО НЕ РАВНЯЙСЯ!");
+        sleep();
+        System.out.println("\n\t\t\t\t\t\t\t\t\t     *********************************");
+
+        System.out.println("\n\n");
+        java.awt.Toolkit.getDefaultToolkit().beep(); 
+        sleep();
+        System.out.println(callBack("HAPPY   "));
+        sleep();
+
+        System.out.println("\t\t\t\t\t\t\t HHHH     HHHH          AA          PPPPPPPPPPP  PPPPPPPPPPP   YY           YY ");
+        System.out.println("\t\t\t\t\t\t\t HHHH     HHHH         AAAA         PP       PP  PP       PP    YY         YY ");
+        System.out.println("\t\t\t\t\t\t\t HHHH     HHHH        AA  AA        PP       PP  PP       PP     YY       YY ");
+        System.out.println("\t\t\t\t\t\t\t HHHHHHHHHHHHH       AA    AA       PP       PP  PP       PP      YY     YY  ");
+        System.out.println("\t\t\t\t\t\t\t HHHHHHHHHHHHH      AA      AA      PPPPPPPPPPP  PPPPPPPPPPP       YYYYYYY ");
+        System.out.println("\t\t\t\t\t\t\t HHHH     HHHH     AAAAAAAAAAAA     PP           PP                   YY ");
+        System.out.println("\t\t\t\t\t\t\t HHHH     HHHH    AA          AA    PP           PP                  YY ");
+        System.out.println("\t\t\t\t\t\t\t HHHH     HHHH   AA            AA   PP           PP                 YY ");
+
+        System.out.println("\n");
+        sleep();
+        sleep();
+        System.out.println(callBack("FUNNY   "));
+        System.out.println("\n");
+        sleep();
+        sleep();
+
+        System.out.println("\t\t\t\t\tBBBBBBBBBBB  IIIIIIIIIIIII RRRRRRRRRRRR  TTTTTTTTTTTTTT HHHH       HHHH  DDDDDDDDDDD        AA   YY          YY ");
+        System.out.println("\t\t\t\t\tBB        BB      II       RR        RR        TT       HHHH       HHHH  DD         D      AAAA   YY        YY ");
+        System.out.println("\t\t\t\t\tBB        BB      II       RR       RR         TT       HHHH       HHHH  DD          D    AA  AA   YY      YY ");
+        System.out.println("\t\t\t\t\tBBBBBBBBBB        II       RRRRRRRRRRR         TT       HHHHHHHHHHHHHHH  DD          D   AA    AA   YYYYYYYY ");
+        System.out.println("\t\t\t\t\tBB        BB      II       RR       RR         TT       HHHH       HHHH  DD          D  AAAAAAAAAA       YY ");
+        System.out.println("\t\t\t\t\tBB        BB      II       RR        RR        TT       HHHH       HHHH  DD         D  AA        AA     YY  ");
+        System.out.println("\t\t\t\t\tBBBBBBBBBBB  IIIIIIIIIIIII RR         RR       TT       HHHH       HHHH  DDDDDDDDDDD  AA          AA   YY  ");
+
+        System.out.println("\n");
+        sleep();
+        sleep();
+        System.out.println(callBack("FRIENDLY"));
+        System.out.println("\n");
+        sleep();
+        sleep();
+
+        System.out.println("\t\t\t\t\t\t\t                                                                             ЙЙЙ");
+        System.out.println("\t\t\t\t\t\t\t       AA        HH      HH         ДД         PPPPPPPP    EEEEEEEEEE   ЙЙ       ЙЙЙ");
+        System.out.println("\t\t\t\t\t\t\t      AAAA       HH      HH        ДДДД        PP     PP   EE           ЙЙ     ЙЙЙЙЙ");
+        System.out.println("\t\t\t\t\t\t\t     AA  AA      HH      HH       ДД  ДД       PP      PP  EE           ЙЙ    ЙЙ  ЙЙ");
+        System.out.println("\t\t\t\t\t\t\t    AA    AA     HHHHHHHHHH      ДД    ДД      PP     PP   EEEEEEEEEE   ЙЙ   ЙЙ   ЙЙ");
+        System.out.println("\t\t\t\t\t\t\t   AAAAAAAAAA    HH      HH     ДД      ДД     PPPPPPPP    EE           ЙЙ  ЙЙ    ЙЙ");
+        System.out.println("\t\t\t\t\t\t\t  AA        AA   HH      HH    ДД        ДД    PP          EE           ЙЙ ЙЙ     ЙЙ");
+        System.out.println("\t\t\t\t\t\t\t AA          AA  HH      HH  ДДДДДДДДДДДДДДДД  PP          EE           ЙЙЙЙ      ЙЙ");
+        System.out.println("\t\t\t\t\t\t\tAA            AA HH      HH     ДД      ДД     PP          EEEEEEEEEE   ЙЙЙ       ЙЙ");
+
+        java.awt.Toolkit.getDefaultToolkit().beep();
+        sleep();
+        System.out.println("\n");
+        System.out.println(callBack("HEALTHY "));
+        java.awt.Toolkit.getDefaultToolkit().beep();
+        sleep();
+        System.out.println("\n");
+        System.out.println(callBack("   FIT  "));
+        java.awt.Toolkit.getDefaultToolkit().beep();
+        sleep();
+        System.out.println("");
+
+        System.out.println("\t\t\t\t\t\t LL             IIIIIIIIIIIIIII  VV            VV  EEEEEEEEEE ");
+        System.out.println("\t\t\t\t\t\t LL                    II         VV          VV   EE         ");
+        System.out.println("\t\t\t\t\t\t LL                    II          VV        VV    EE         ");
+        System.out.println("\t\t\t\t\t\t LL                    II           VV      VV     EEEEEEEEEE ");
+        System.out.println("\t\t\t\t\t\t LL                    II            VV    VV      EEEEEEEEEE ");
+        System.out.println("\t\t\t\t\t\t LL                    II             VV  VV       EE         ");
+        System.out.println("\t\t\t\t\t\t LL                    II              VVVV        EE         ");
+        System.out.println("\t\t\t\t\t\t LLLLLLLLLLLLL  IIIIIIIIIIIIIII         VV         EEEEEEEEEE ");
+
+		sleep();
+        System.out.println("\n\n");
+
+        System.out.println("\t\t\t\t\t\t\t\t\t\t\t\t LL               OOOO       NN       NN        GGG  ");
+        System.out.println("\t\t\t\t\t\t\t\t\t\t\t\t LL             OO    OO     NNNN     NN     GG    GG ");
+        System.out.println("\t\t\t\t\t\t\t\t\t\t\t\t LL           OO        OO   NN NN    NN    GG        ");
+        System.out.println("\t\t\t\t\t\t\t\t\t\t\t\t LL          OO          OO  NN  NN   NN    GG        ");
+        System.out.println("\t\t\t\t\t\t\t\t\t\t\t\t LL          OO          OO  NN   NN  NN    GG        ");
+        System.out.println("\t\t\t\t\t\t\t\t\t\t\t\t LL           OO        OO   NN    NN NN     GG   GGGG");
+        System.out.println("\t\t\t\t\t\t\t\t\t\t\t\t LL             OO     OO    NN     NNNN       GGG  GG");
+        System.out.println("\t\t\t\t\t\t\t\t\t\t\t\t LLLLLLLLLLLLL    OOOO       NN       NN            GG");
+
+		java.awt.Toolkit.getDefaultToolkit().beep();
+		sleep();
+        sleep();
+        System.out.println("\n");
+        System.out.println(callBack("BEST    "));
+        java.awt.Toolkit.getDefaultToolkit().beep();
+        sleep();
+        System.out.println("\n");
+        System.out.println(callBack("YOURSELF"));
+        java.awt.Toolkit.getDefaultToolkit().beep();
+        sleep();
+
+        System.out.println("\n\n");
+        java.awt.Toolkit.getDefaultToolkit().beep();
+        sleep();
+        sleep();
+    }
+
+
+    static void sleep() {
+        try {
+
+            Thread.sleep(1000);
+
+        } catch (InterruptedException e) {
+
+        }
+    }
+
+
+    public static String callBack(String input) {
+
+        int[] array = { 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 0, 1,
+                0, 0, 1, 1, 4, 5, 2, 3, 1, 0, 0, 1,
+
+                0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0 };
+
+        StringBuffer sb = new StringBuffer();
+
+        for (int i = 0; i < array.length; i++) {
+
+            if (i % 7 == 0)
+
+                sb.append("  \n\t\t\t\t\t\t\t\t\t\t");
+
+            if (array[i] == 0)
+
+                sb.append("   ");
+                
+            else if (array[i] == 4)
+
+                sb.append("  ");
+
+            else if (array[i] == 5)
+
+                sb.append("  BE ");
+
+            else if (array[i] == 2)
+
+                sb.append(input);
+
+            else if (array[i] == 3)
+
+                sb.append("");
+
+            else
+
+                sb.append("  " + "\u2764");
+
+        }
+
+        return sb.toString();
+
+    }
+}  
